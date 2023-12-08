1cd3fc82ba0080ce646ddbc6b890ee326062807a
Nevania <140127237+Nevania@users.noreply.github.com>
2023-12-08 05:41:59 +0100

    Agregar archivos de proyecto.

---
+++ MauiApp1/mainpage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    internal class mainpage : TabbedPage
+    {
+        private void Suma_Clicked(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(valor1_suma.Text);
+            int valor2 = int.Parse(valor2_suma.Text);
+            int valor3 = int.Parse(valor3_suma.Text);
+            int resultado = valor1 + valor2 + valor3;
+            resultado_suma.Text = "Resultado: " + resultado;
+        }
+
+        private void Resta_Clicked(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(valor1_resta.Text);
+            int valor2 = int.Parse(valor2_resta.Text);
+            int resultado = valor1 - valor2;
+            resultado_resta.Tex = "Resultado: " + resultado;
+        }
+
+        private void Multiplicacion_Clicked(object sender, EventArgs e)
+        {
+            int valor1 = int.Parse(valor1_multiplicacion.Text);
+            int valor2 = int.Parse(Valor2_multiplicacion.Text);
+            int resultado = valor1 * valor2;
+            resultado_multiplicacion.Text = "Resultado:" * resultado;
+    }
+   
